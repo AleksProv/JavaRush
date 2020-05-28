466d1f6b9fe483511d0261c33682ddf3e848cab9
AleksProv <cmcgidhub@gmail.com>
2020-05-28 13:18:35 +0300

    Проект от 2020.05.28

---
+++ src/level4/lectore_10/Solution_1.java
@@ -0,0 +1,13 @@
+package level4.lectore_10;
+
+//вывести на экран числа от 1 до 10 используя while
+
+public class Solution_1 {
+    public static void main (String[] args) {
+        int i = 0;
+        while (i < 10 ) {
+            i++;
+            System.out.println (i);
+        }
+    }
+}
---
+++ src/level4/lectore_10/Solution_5.java
@@ -0,0 +1,23 @@
+package level4.lectore_10;
+
+//вывести на экран таблицу умножения 10*10 используя while
+
+public class Solution_5 {
+    public static void main (String[] args) {
+        int i = 0;
+        int j = 0;
+        int namb;
+        int mno = 0;
+        while (i < 10) {
+            i++;
+            mno++;
+            while (j < 10) {
+                j++;
+                namb = mno * j;
+                System.out.print (namb + " ");
+            }
+            System.out.println (" ");
+            j = 0;
+        }
+    }
+}
