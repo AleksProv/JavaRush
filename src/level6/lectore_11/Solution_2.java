9f60d2bd0085a31052759f0a2dd82fda01d47648
AleksProv <cmcgidhub@gmail.com>
2020-06-08 16:31:06 +0300

    проект от 2020.06.08

--- src/level6/lectore_11/Solution_2.java
+++ src/level6/lectore_11/Solution_2.java
@@ -4,22 +4,19 @@ package level6.lectore_11;
 Блокнот для новых идей.
  */
 
-import sun.applet.Main;
-
 public class Solution_2 {
 
     public static void main(String[] args) {
         printIdea(new Idea());
     }
 
-    public static void printIdea(Idea idea){
-        System.out.println(idea);
+    public static void printIdea(Idea idea) {
+        System.out.println(idea.getDescription ());
     }
-
 
     public static class Idea {
         public String getDescription() {
-            return "Блокнот для новых идей";
+             return "Блокнот для новых идей";
         }
     }
 
