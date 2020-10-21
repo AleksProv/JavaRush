03a7657f43c326c90ef7a7366fbcf0e13e6a457f
AleksProv <cmcgidhub@gmail.com>
2020-10-21 12:32:46 +0300

    Проект от 2020.10.21

---
+++ src/level10/lectore_11/Solution_10.java
@@ -0,0 +1,34 @@
+package level10.lectore_11;
+/*
+Задача по алгоритмам I Java Syntax: 10 уровень, 11 лекция
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_10 {
+    public static void main (String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            array[i] = Integer.parseInt (reader.readLine ());
+        }
+        sort(array);
+        System.out.println (array[9]);
+        System.out.println (array[10]);
+    }
+
+    public static void sort(int[] array){
+        int arrSort;
+        int index;
+        for (int i = 0; i < array.length; i++) {
+            arrSort = array[i];
+            index = i;
+            while (index > 0 && array[index-1] > arrSort) {
+                array[index] = array[index-1];
+                index-- ;
+                array[index] = arrSort;
+            }
+        }
+    }
+}
